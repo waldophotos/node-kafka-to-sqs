3a03fe6f7b3c844361eb7f1e47c5894cdb1a2bc8
Thanasis Polychronakis <thanpolas@gmail.com>
2017-01-11 12:35:46 +0200

    Scaffold testing

--- test/spec/base.test.js
+++ test/spec/base.test.js
@@ -4,13 +4,30 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const awesomeLib = require('../..');
+const KafkaToSqs = require('../..');
 
 describe('Base API Surface', function() {
-  it('should expose expected methods', function(){
-    expect(awesomeLib).to.have.keys([
-      'awesome',
-      'method',
-    ]);
+  describe('Nominal behaviors', function() {
+    it('should expose a constructor', function() {
+      expect(KafkaToSqs).to.be.a('function');
+    });
+    it('instance should have expected methods', function() {
+      const kafkaToSqs = new KafkaToSqs();
+
+      expect(kafkaToSqs.init).to.be.a('function');
+    });
+  });
+  describe('Erroneous behaviors', function() {
+    beforeEach(function() {
+      this.kafkaToSqs = new KafkaToSqs();
+    });
+    it('should not allow init without an object', function() {
+      expect(this.kafkaToSqs.init).to.throw(TypeError);
+    });
+    it('should not allow init without anything defined', function() {
+      const kafkaInit = this.kafkaToSqs.init.bind(this.kafkaToSqs, {});
+
+      expect(kafkaInit).to.throw(TypeError);
+    });
   });
 });
